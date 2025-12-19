a38d9aa1a74a061d392cdf3ee8a9d17aa077587a
codergangganesh <mannamganeshbabu8@gmail.com>
2025-12-19 14:32:24 +0530

    BINARY SEARCH TREES\Largest BST in Binary Tree.java

---
+++ BINARY SEARCH TREES/Largest BST in Binary Tree.java
@@ -0,0 +1,45 @@
+// class Node
+// {
+//     int data;
+//     Node left, right;
+
+//     public Node(int d)
+//     {
+//         data = d;
+//         left = right = null;
+//     }
+// }
+
+class Solution {
+
+    static int[] maxBST(Node root)
+    {
+        if(root == null)
+        {
+            return new int[]{0, Integer.MAX_VALUE, Integer.MIN_VALUE}; 
+        }
+
+        int[] left = maxBST(root.left);
+        int[] right = maxBST(root.right); 
+
+        if(root.data > left[2] && root.data < right[1])
+        {
+            return new int[]{
+                left[0] + right[0] + 1,
+                Math.min(root.data, left[1]),
+                Math.max(root.data, right[2])
+            };
+        }
+        else
+            return new int[]{
+                Math.max(left[0], right[0]),
+                Integer.MIN_VALUE,
+                Integer.MAX_VALUE
+            };
+    }
+
+    static int largestBst(Node root) {
+        int[] ans = maxBST(root);
+        return ans[0];
+    }
+}
