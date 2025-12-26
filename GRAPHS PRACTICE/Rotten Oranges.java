5f3889733dc9fe0ebdf145b494bb08b8f321b266
codergangganesh <mannamganeshbabu8@gmail.com>
2025-12-26 09:51:43 +0530

    GRAPHS PRACTICE\Rotten Oranges.java

--- GRAPHS PRACTICE/Rotten Oranges.java
+++ GRAPHS PRACTICE/Rotten Oranges.java
@@ -23,6 +23,9 @@ class Solution {
                 }
             }
         }
-        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++) 
+                if(A[i][j] == 1) return -1;
+        return mx;
     }
 }
