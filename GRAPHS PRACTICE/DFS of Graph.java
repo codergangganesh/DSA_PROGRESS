9d295e0498252ec2db033d5c86067da099bab82b
codergangganesh <mannamganeshbabu8@gmail.com>
2025-12-26 09:53:50 +0530

    GRAPHS PRACTICE\DFS of Graph.java

--- GRAPHS PRACTICE/DFS of Graph.java
+++ GRAPHS PRACTICE/DFS of Graph.java
@@ -6,14 +6,5 @@ class Solution {
         dfsUtil(0, adj, isVisited, ans);
         return ans;
     }
-    private void dfsUtil(int node, ArrayList<ArrayList<Integer>> adj,boolean[] isVisited, ArrayList<Integer> ans) {
-        isVisited[node] = true;
-        ans.add(node);
-        
-        for (int a : adj.get(node)) {
-            if (!isVisited[a]) {
-                dfsUtil(a, adj, isVisited, ans);
-            }
-        }
-    }
+    
 }
