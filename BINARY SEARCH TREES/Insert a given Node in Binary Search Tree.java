caa63e1746a7e1712767260761a517628a2e0bc5
codergangganesh <mannamganeshbabu8@gmail.com>
2025-12-27 13:26:32 +0530

    BINARY SEARCH TREES\Insert a given Node in Binary Search Tree.java

--- BINARY SEARCH TREES/Insert a given Node in Binary Search Tree.java
+++ BINARY SEARCH TREES/Insert a given Node in Binary Search Tree.java
@@ -15,8 +15,7 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null)
-            return new TreeNode(val);
+        
         if(val<root.val)
             root.left=insertIntoBST(root.left,val);
         if (val>root.val)
